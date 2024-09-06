b242e80329fd0cdf0a9d90446b716a974daa711d
joseGomez <jose_armando_za@hotmail.com>
2024-09-05 21:55:47 -0600

    carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+  ngAfterViewInit(): void {
+    setInterval( () => {
+      const carouselElement = document.querySelector('#blog-carousel');
+      if(carouselElement) {
+        (window as any).$(`#blog-carousel`).carousel('next');
+      }
+    }, 20000)
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
